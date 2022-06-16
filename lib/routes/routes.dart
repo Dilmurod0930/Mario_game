32365064169931fbfa0ed34362c4cf58bfb59457
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2022-06-16 23:04:31 +0500

    Screens page add

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mario_game/screens/mario_game_screnns.dart';
 
 class MyRoutes {
   static final MyRoutes _instants = MyRoutes._init();
@@ -8,8 +9,7 @@ class MyRoutes {
     var args = settings.arguments;
     switch (settings.name) {
       case "/home":
-        return MaterialPageRoute(builder: (context) => ());
-  
+        return MaterialPageRoute(builder: (context) => MainPage());
     }
   }
 }
